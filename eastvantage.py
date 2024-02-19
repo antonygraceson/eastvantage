b8e2b8f661f24d3e497d35e743c009f9415abfcf
antonygraceson <48781871+antonygraceson@users.noreply.github.com>
2024-02-20 00:33:35 +0530

    Update eastvantage.py

--- eastvantage.py
+++ eastvantage.py
@@ -2,15 +2,16 @@ import sqlite3
 import pandas as pd
 
 try:
-    conn = sqlite3.connect(r"C:\Users\anton\Pictures\eastvantage\EastVantage.db")
+    conn = sqlite3.connect(r"C:\Users\anton\Pictures\eastvantage\Data_Engineer_ETL_Assignment.db")
     sql_query = """
-    SELECT c.customer_id AS Customer,c.age AS Age,item_name AS Item,sum(o.quantity) AS Quantity FROM Orders as o 
-    JOIN Customer c on c.customer_id = s.customer_id
-    JOIN Sales s on s.sales_id = o.sales_id
-    JOIN Items i on i.item_id = o.item_id
+    SELECT c.customer_id AS Customer,c.age AS Age,i.item_name AS Item,sum(o.quantity) AS Quantity FROM orders as o 
+    JOIN customers c on c.customer_id = s.customer_id
+    JOIN sales s on s.sales_id = o.sales_id
+    JOIN items i on i.item_id = o.item_id
     WHERE o.quantity IS NOT NULL AND c.age BETWEEN 18 AND 35  
-    GROUP BY c.customer_id
+    GROUP BY c.customer_id, c.age, i.item_name
     HAVING Quantity > 0
+    ORDER BY c.customer_id, i.item_name;
     """
     df = pd.read_sql_query(sql_query, conn)
     df.to_csv('output.csv', index=False)
