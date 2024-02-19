2114a8958c016aafaa53f682976172351701cc2f
antonygraceson <48781871+antonygraceson@users.noreply.github.com>
2024-02-20 00:33:47 +0530

    Update eastvantage.sql

--- eastvantage.sql
+++ eastvantage.sql
@@ -1,7 +1,8 @@
-SELECT c.customer_id AS Customer,c.age AS Age,item_name AS Item,sum(o.quantity) AS Quantity FROM Orders as o
-    JOIN Customer c on c.customer_id = s.customer_id
-    JOIN Sales s on s.sales_id = o.sales_id
-    JOIN Items i on i.item_id = o.item_id
-    WHERE o.quantity IS NOT NULL AND c.age BETWEEN 18 AND 35
-    GROUP BY c.customer_id
-    HAVING Quantity > 0
+SELECT c.customer_id AS Customer,c.age AS Age,i.item_name AS Item,sum(o.quantity) AS Quantity FROM orders as o
+JOIN customers c on c.customer_id = s.customer_id
+JOIN sales s on s.sales_id = o.sales_id
+JOIN items i on i.item_id = o.item_id
+WHERE o.quantity IS NOT NULL AND c.age BETWEEN 18 AND 35
+GROUP BY c.customer_id, c.age, i.item_name
+HAVING Quantity > 0
+ORDER BY c.customer_id, i.item_name;
